c1da38b1427d89f676df372852aec56ad14eeb90
jisu3316 <95600042+jisu3316@users.noreply.github.com>
2022-05-05 00:27:23 +0900

    Update SpringConfig.java

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,38 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
 
 import hello.hellospring.sevice.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
     @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//       return new MemoryMemberRepository();
+//       return new JdbcMemberRepository(dataSource);
+//       return  new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+ //   }
 }
